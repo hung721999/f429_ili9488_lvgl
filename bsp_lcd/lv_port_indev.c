c26639f5304a01e35e17158f124df73db204c9fc
Nguyen Nhu Hung <hung.nguyen-nhu@internal.banvien.com>
2024-09-16 13:34:26 +0700

    add calib touchscreen code

--- bsp_lcd/lv_port_indev.c
+++ bsp_lcd/lv_port_indev.c
@@ -26,6 +26,8 @@
 
 static void touchpad_init(void);
 static void touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data);
+static bool touchpad_is_pressed(void);
+static void touchpad_get_xy(lv_coord_t *x, lv_coord_t *y);
 
 /**********************
  *  STATIC VARIABLES
@@ -90,9 +92,8 @@ static void touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data) {
 	static lv_coord_t last_y = 0;
 
 	/*Save the pressed coordinates and the state*/
-	if (ili9488_ts_DetectTouch()) {
-//		touchpad_get_xy(&last_x, &last_y);
-		ili9488_ts_GetXY(&last_x, &last_y);
+	if (touchpad_is_pressed()) {
+		touchpad_get_xy(&last_x, &last_y);
 		data->state = LV_INDEV_STATE_PR;
 	} else {
 		data->state = LV_INDEV_STATE_REL;
@@ -101,6 +102,37 @@ static void touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data) {
 	/*Set the last pressed coordinates*/
 	data->point.x = last_x;
 	data->point.y = last_y;
+}
+
+/*Return true is the touchpad is pressed*/
+static bool touchpad_is_pressed(void) {
+	/*Your code comes here*/
+	return ili9488_ts_DetectTouch();
+}
+
+/*Get the x and y coordinates if the touchpad is pressed*/
+static void touchpad_get_xy(lv_coord_t *x, lv_coord_t *y) {
+	/*Your code comes here*/
+	int16_t a, b;
+	int32_t x1, y1, x2, y2;
+	ili9488_ts_GetXY(&a, &b);
+	x1 = a;
+	y1 = b;
+	x2 = (ts_cindex[1] * x1 + ts_cindex[2] * y1 + ts_cindex[3]) / ts_cindex[0];
+	y2 = (ts_cindex[4] * x1 + ts_cindex[5] * y1 + ts_cindex[6]) / ts_cindex[0];
+
+	if (x2 < 0)
+		x2 = 0;
+	else if (x2 >= ILI9488_LCD_PIXEL_HEIGHT)
+		x2 = ILI9488_LCD_PIXEL_HEIGHT - 1;
+
+	if (y2 < 0)
+		y2 = 0;
+	else if (y2 >= ILI9488_LCD_PIXEL_WIDTH)
+		y2 = ILI9488_LCD_PIXEL_WIDTH - 1;
+
+	*x = (lv_coord_t) x2;
+	*y = (lv_coord_t) y2;
 }
 
 #else /*Enable this file at the top*/
